0cb5a03ba9d800943786634ecfde7e31e4e61376
mertsaygi <mertsaygi@gmail.com>
2022-03-11 12:04:58 +0300

    Release 0.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { spinnakerPlugin } from './plugin';
 export * from './api';
 export * from './integration';
+export * from './extensions';
+export { SpinnakerPipelinesStatusCard } from './components/SpinnakerPipelineStatusCard';
